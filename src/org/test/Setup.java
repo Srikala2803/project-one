2eb9ca0de42aadaa91eb693a85cf4128f73e83ec
Srikala2803 <choudarysri.an@gmail.com>
2023-02-21 18:00:30 -0600

    CODING DONE

--- src/org/test/Setup.java
+++ src/org/test/Setup.java
@@ -5,6 +5,8 @@ public class Setup {
 	public static void main(String[] args) {
 		
 		System.out.println("AUTOMATION TESTING");
+		System.out.println("SELENIUM WITH JAVA");
+		System.out.println("JAVA SRIPT EXECUTOR");
 	}
 
 }
